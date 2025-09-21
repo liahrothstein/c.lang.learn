2ab789d4e032f303708c6e9a7321ca740054be83
liahrothstein <liahrothstein@gmail.com>
2025-09-21 18:37:43 +0300

    feat: complete two tasks

---
+++ consts/boolean.c
@@ -0,0 +1,2 @@
+const int true = 1;
+const int false = 0; /*immutability boolean values*/
---
+++ tasks/task1.c
@@ -0,0 +1,36 @@
+/*
+Task 1 - Develop programm processing a sequence of numbers with using "while" and without arrays
+Author: Student Novikov D.A.
+Date: 19.09.25
+Ver: 01
+*/
+
+#include <stdio.h> /*lib input and output*/
+
+#include "../consts/boolean.c"
+
+int main() {
+    int currentNum;
+    int sum;
+
+    printf("Please type some number sequence\n");
+    printf("For exit type number multiple of 8\n");
+
+    while (true) {
+        scanf("%d", &currentNum);
+        
+        if (currentNum > 9) {
+            sum += currentNum;
+        };
+
+        if ((currentNum % 8) == 0) {
+            break;
+        }
+    };
+    
+    if (sum != 0) {
+        printf("Sum all two-digit numbers: %d\n", sum);
+    };
+
+    return 0;
+}
