5bc9032045ddf16c7cbc5f65359a4a70a33513e2
liahrothstein <liahrothstein@gmail.com>
2025-10-06 18:47:12 +0300

    fix: change if condition

--- task1.c
+++ task1.c
@@ -13,14 +13,16 @@ int main() {
     int currentNum;
     int sum;
 
-    printf("Hello user, this programm process a sequence of numbers\n\n");
+    printf("Hello user, this program calculates the sum of two-digit numbers\n\n");
     printf("Please type some number sequence\n");
     printf("For exit type number multiple of 8\n");
 
     while (true) {
         scanf("%d", &currentNum);
         
-        if (currentNum > 9) {
+        if (currentNum > 9 && currentNum < 100) { /*check for positive numbers*/
+            sum += currentNum;
+        } else if (currentNum < -9 && currentNum > -100) { /*check for negative numbers*/
             sum += currentNum;
         };
 
